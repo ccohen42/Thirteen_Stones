4ff47f609efc84ab62e34c9f21f53b57fed0e7d8
Chaya-Sara-Milstein <ccohen42@student.touro.edu>
2024-12-15 17:56:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/thirteenstones/activities/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.thirteenstones.activities;
+
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY;
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.preference.PreferenceManager;
+
+import com.example.thirteenstones.R;
+import com.example.thirteenstones.lib.Utils;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false);
+        Utils.setNightModeOnOffFromPreferenceValue(getApplicationContext(),
+                getString(R.string.night_mode_key));
+
+        startActivity (new Intent(getApplicationContext (), MainActivity.class));
+        finish ();
+    }
+}
